33d2d3ab16332190f5f5262a68fb12c5e2d0a745
piotras <pioroznowski@gmail.com>
2018-11-15 13:47:18 +0100

    heh

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+from django.conf.urls import include, url
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
@@ -11,7 +11,7 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-    path(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
-    path(r'^comment/(?P<pk>\d+)/approve/$', views.comment_approve, name='comment_approve'),
-    path(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
+    url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
+    url(r'^comment/(?P<pk>\d+)/approve/$', views.comment_approve, name='comment_approve'),
+    url(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
     ]
